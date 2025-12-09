1785b8d9763f429ca2b5956337fa191096c7f2bd
xelwarto <xelwarto.pub@gmail.com>
2025-12-08 21:32:31 -0600

    Day 1 complete

--- cmd/days/1.go
+++ cmd/days/1.go
@@ -3,6 +3,8 @@ package days
 import (
 	"code/util"
 	"fmt"
+	"strconv"
+	"strings"
 )
 
 type Day1 int
@@ -10,22 +12,98 @@ type Day1 int
 var day1 = Day1(1)
 
 func (d Day1) Part1() (string, error) {
-	_, err := util.File2Array("inputs/day1_part1.txt")
+	s, err := util.File2Array("inputs/day1_part1.txt")
 	if err != nil {
 		return "", err
 	}
 
 	total := 0
+	pos := 50
+
+	for _, line := range s {
+		dir := string(line[0])
+		snum := strings.TrimLeft(line, "LR")
+		num, err := strconv.ParseInt(snum, 10, 64)
+		if err != nil {
+			return "", err
+		}
+
+		// fmt.Printf("%s / %s / %v\n", line, dir, int(num))
+
+		if dir == "L" {
+			for x := 0; x < int(num); x++ {
+				pos--
+				if pos < 0 {
+					pos = 99
+				}
+			}
+		}
+
+		if dir == "R" {
+			for x := 0; x < int(num); x++ {
+				pos++
+				if pos > 99 {
+					pos = 0
+				}
+			}
+		}
+
+		// fmt.Println(pos)
+
+		if pos == 0 {
+			total++
+		}
+	}
+
 	return fmt.Sprintf("%v", total), nil
 }
 
 func (d Day1) Part2() (string, error) {
-	_, err := util.File2Array("inputs/day1_part2.txt")
+	s, err := util.File2Array("inputs/day1_part2.txt")
 	if err != nil {
 		return "", err
 	}
 
 	total := 0
+	pos := 50
+
+	for _, line := range s {
+		dir := string(line[0])
+		snum := strings.TrimLeft(line, "LR")
+		num, err := strconv.ParseInt(snum, 10, 64)
+		if err != nil {
+			return "", err
+		}
+
+		// fmt.Printf("%s / %s / %v\n", line, dir, int(num))
+
+		if dir == "L" {
+			for x := 0; x < int(num); x++ {
+				pos--
+				if pos < 0 {
+					pos = 99
+				}
+				if pos == 0 {
+					total++
+				}
+			}
+		}
+
+		if dir == "R" {
+			for x := 0; x < int(num); x++ {
+				pos++
+				if pos > 99 {
+					pos = 0
+				}
+				if pos == 0 {
+					total++
+				}
+			}
+		}
+
+		// fmt.Println(pos)
+	}
+
 	return fmt.Sprintf("%v", total), nil
 }
 
