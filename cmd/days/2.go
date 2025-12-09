b7776741311e675f6b7b54d05c9d317cd7daed61
xelwarto <xelwarto.pub@gmail.com>
2025-12-08 22:11:31 -0600

    Day 2 part 1 complete

--- cmd/days/2.go
+++ cmd/days/2.go
@@ -1,7 +1,10 @@
 package days
 
 import (
+	"code/util"
 	"fmt"
+	"strconv"
+	"strings"
 )
 
 type Day2 int
@@ -9,7 +12,56 @@ type Day2 int
 var day2 = Day2(2)
 
 func (d Day2) Part1() (string, error) {
+	s, err := util.File2Array("inputs/day2_part1.txt")
+	if err != nil {
+		return "", err
+	}
 	total := 0
+	line := s[0]
+	products := strings.Split(line, ",")
+	for _, p := range products {
+		if strings.Contains(p, "-") {
+			nums := strings.Split(p, "-")
+			if len(nums) == 2 {
+				start, err := strconv.ParseInt(nums[0], 10, 64)
+				if err != nil {
+					return "", err
+				}
+
+				end, err := strconv.ParseInt(nums[1], 10, 64)
+				if err != nil {
+					return "", err
+				}
+
+				// fmt.Printf("%v - %v\n", start, end)
+
+				for x := start; x <= end; x++ {
+					sx := strconv.Itoa(int(x))
+					if (len(sx) % 2) == 0 {
+						hl := len(sx) / 2
+
+						s_start := sx[0:hl]
+						count := strings.Count(sx, s_start)
+
+						if count == 2 {
+							total = total + int(x)
+						}
+
+						if count > 2 {
+							// fmt.Printf("%s - %v / %s / %v\n", sx, hl, s_start, count)
+						}
+
+						// fmt.Printf("%s - %v / %s / %v\n", sx, hl, s_start, count)
+					}
+				}
+			} else {
+				return "", fmt.Errorf("invalid input")
+			}
+		} else {
+			return "", fmt.Errorf("invalid input")
+		}
+	}
+
 	return fmt.Sprintf("%v", total), nil
 }
 
